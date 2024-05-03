c07bf8d956b1ffd19aa8a31f720cdbc277e726c7
seetha-devi <seethadevi322001@gmail.com>
2024-05-02 22:24:47 -0700

    change text style in product card

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -253,7 +253,7 @@ const Filters = ({ onFilter }) =>{
                 handleFilter()
                 dispatch(togglePopup());
               }}>Apply Filters</button>
-
+            
            
           </div>
 
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../features/cart/cartSlice';
-// import styles from '../styles/ProductCard.module.css';
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
@@ -14,10 +13,12 @@ const ProductCard = ({ product }) => {
     <div className="productCard">
       <img src={product.imageURL} alt={product.name} className="productImage" />
     <div className="productDetail">
+     <div>
       <h3 className="productName">{product.name}</h3>
       <p className="productPrice">Rs.{product.price}</p>
      </div>
       <button onClick={handleAddToCart} className="addToCartButton">Add to Cart</button>
+    </div>
     </div>
   );
 };
